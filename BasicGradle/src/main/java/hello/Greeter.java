5bbd791d423802c33ccf5ca5a461b8e822a12a17
Maelstroms <workpartystudy@yahoo.com>
2014-11-30 11:06:44 -0500

    first commit

---
+++ BasicGradle/src/main/java/hello/Greeter.java
@@ -0,0 +1,10 @@
+package hello;
+
+/**
+ * Created by grantmerrill on 11/28/14.
+ */
+public class Greeter {
+    public String sayHello() {
+        return "Hello world!";
+    }
+}
---
+++ BasicGradle/src/main/java/hello/HelloWorld.java
@@ -0,0 +1,16 @@
+package hello;
+
+/**
+ * Created by grantmerrill on 11/28/14.
+ */
+import org.joda.time.LocalTime;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        LocalTime currentTime = new LocalTime();
+        System.out.println("The current local time is: " + currentTime);
+
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
